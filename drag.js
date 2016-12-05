86d3638739b1e01f1c2b0e129b8a3dee4c514261
IFECoder <245024598@qq.com>
2016-12-05 13:54:56 +0800

    finish this drag

--- drag.js
+++ drag.js
@@ -1,7 +1,20 @@
 function drag(id) {
 	var obj = document.getElementById(id);
 	var disx=0;
-	obj.onmousedown=function () {
-		
+	var disy=0;
+	obj.onmousedown=function (ev) {
+		disx=ev.clientX-this.offsetLeft;
+		disy=ev.clientY-this.offsetTop;
+		document.onmousemove=function (ev) {
+			obj.style.left=ev.clientX-disx+'px';
+			obj.style.top=ev.clientY-disy+'px';
+			return false;
+		};
+		document.onmouseup=function () {
+			document.onmousemove=null;
+			document.onmouseup=null;
+		}
+		return false;
 	}
+	
 }
